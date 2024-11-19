d9b085a5108f26cf09344936152a5aa5ba837bb2
Deepak Kumar Raj <76236564+ideepak9898@users.noreply.github.com>
2024-11-19 12:29:11 +0530

    Update sendmail.py

--- sendmail.py
+++ sendmail.py
@@ -1,16 +1,18 @@
 # --------------------------------------------------------------------------
 # import yagmail
 # yagmail.register('SenderMail@gmail.com', '16 Character Password')
+
+# To get 16 Character Password follow this link 'https://docs.google.com/document/d/1i0tNNIsRysp_Z8BfgwyIJK-B2A1fikgnHYgatECI6jA/edit?usp=sharing'
 # --------------------------------------------------------------------------
 
 import yagmail
 import os
 
-receiver = "deepakraj33333@gmail.com"
+receiver = "ReceiverMail@gmail.com"
 body = "Attendence File"
 filename = "Attendance"+os.sep+"Attendance.csv" 
 
-yag = yagmail.SMTP('seema.dubai.nepal@gmail.com')
+yag = yagmail.SMTP('SenderMail@gmail.com')
 
 try:
     yag.send(
